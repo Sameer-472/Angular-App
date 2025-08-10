5fb6f79665a9b4bb5a0bcf4382b9e46c9e6c4d1b
Sameer Khan <muhammadsameer2016@yahoo.com>
2025-08-10 03:38:37 -0700

    Understand basic of angular | folder structure | Data types | events

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,40 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { SignUp } from './sign-up/sign-up';
+import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login, SignUp, Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('angular-app');
+  // protected readonly title = signal('angular-app');
+  name = "test";
+  age = 30;
+  education = "B.Tech";
+  count: number = 0;
+  handleClick() {
+    console.log("Button clicked");
+  }
+
+  handleIncrement() {
+    this.count++;
+  }
+
+  handleDecrement() {
+    this.count = Math.max(0, this.count - 1);
+  }
+
+  handleReset() {
+    this.count = 0;
+  }
+
+  handleOnChange(event: Event){
+    console.log((event.target as HTMLInputElement).value);
+  }
+
 }
---
+++ src/app/profile/profile.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-profile',
+    imports: [],
+    template: `<h1>Profile Component</h1>` 
+})
+
+export class Profile {
+
+}
