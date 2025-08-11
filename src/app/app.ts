915f853ea7681e68898aee91c6e83902571fbef8
Sameer Khan <muhammadsameer2016@yahoo.com>
2025-08-11 08:24:42 -0700

    understand ng directive | signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, effect, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
 import { SignUp } from './sign-up/sign-up';
 import { Profile } from './profile/profile';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Login, SignUp, Profile],
+  imports: [Login, SignUp, Profile , FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -54,4 +56,69 @@ export class App {
   getValue(item: string) {
     console.log(item);
   }
+
+  // signals and computed signals
+  // signals are reactive variables that can be used to store and manage state in Angular applications.
+  // computed signals are derived from other signals and automatically update when their dependencies change.
+
+  countSignal = signal(0);
+  computedSignal = computed(() => this.countSignal() * 2);
+  updatedSignal = this.computedSignal()
+  displayHeading = false;
+
+
+
+
+  // effect in angular 
+  // effect is a function that allows you to run side effects in response to changes in signals.
+  // It automatically tracks the signals used within its body and re-runs whenever any of those signals change.
+
+  constructor() {
+    effect(() => {
+      console.log("Count signal changed: ", this.countSignal())
+      console.log("Computed signal changed: ", this.computedSignal())
+      if (this.countSignal() > 100) {
+        this.displayHeading = true;
+      }
+    });
+  }
+
+  // for loop contextual variables 
+  // $count , $index , $first , $last , $even , $odd
+  // $count is the total number of items in the array.
+  // $index is the current index of the item in the loop.
+  // $first is true if the current item is the first item in the loop.
+  // $last is true if the current item is the last item in the loop.
+  // $even is true if the current index is even.
+  // $odd is true if the current index is odd. 
+
+  users = ["sameer", "ali", "sana", "sara", "sami", "samiya",];
+
+
+  // Two way binding in Angular
+  // Two-way binding is a powerful feature in Angular that allows you to synchronize data between the component and the template.
+  // It is achieved using the [(ngModel)] directive, which binds a property in the component to an input element in the template.
+
+  studentName = "";
+
+
+  // Todo task here 
+  task= "";
+  taskList: {id: string , task: string}[] = [];
+
+  handleAdd(){
+    this.taskList.push({id: this.task , task: this.task})
+  }
+
+  red= "red";
+
+  // directives in angular 
+  // directive is a class that add additional behavior in angular 
+  // ngIf , ngFor , ngStyle , ngSwitch , ngClass
+
+  // Component directive , 
+  // Structural directive 
+  // Attribute Directive 
+  show = false;
 }
+ 
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,33 @@
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'login',
-  imports: [],
+  imports: [NgIf, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  show = true;
 
+  students = ['sameer', 'peter', 'john']
+
+  complexObj = [
+    {
+      name: 'sameer',
+      age: '26'
+    },
+    {
+      name: 'Test',
+      age: '20'
+    },
+    {
+      name: 'Check',
+      age: '2'
+    },
+  ]
+
+  login = false;
+  block = 1;
+  color = 'blue';
 }
